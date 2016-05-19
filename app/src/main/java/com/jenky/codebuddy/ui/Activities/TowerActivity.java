d91e787de15dc1589e98b5293370db0001591a21
Jtlie <jtlie@live.nl>
2016-05-20 00:32:23 +0200

    tower setup

--- app/src/main/java/com/jenky/codebuddy/ui/Activities/TowerActivity.java
+++ app/src/main/java/com/jenky/codebuddy/ui/Activities/TowerActivity.java
@@ -2,15 +2,82 @@ package com.jenky.codebuddy.ui.activities;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import com.jenky.codebuddy.R;
 
-public class TowerActivity extends Activity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+
+    public class TowerActivity extends Activity {
+
+    private float mx, my;
+    private float curX, curY;
+
+    private ScrollView vScroll;
+    private HorizontalScrollView hScroll;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tower);
+
+        vScroll = (ScrollView) findViewById(R.id.vScroll);
+        hScroll = (HorizontalScrollView) findViewById(R.id.hScroll);
+        scrollDown(vScroll);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float curX, curY;
+
+        switch (event.getAction()) {
+
+            case MotionEvent.ACTION_DOWN:
+                mx = event.getX();
+                my = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                curX = event.getX();
+                curY = event.getY();
+                vScroll.scrollBy((int) (mx - curX), (int) (my - curY));
+                hScroll.scrollBy((int) (mx - curX), (int) (my - curY));
+                mx = curX;
+                my = curY;
+                break;
+            case MotionEvent.ACTION_UP:
+                curX = event.getX();
+                curY = event.getY();
+                vScroll.scrollBy((int) (mx - curX), (int) (my - curY));
+                hScroll.scrollBy((int) (mx - curX), (int) (my - curY));
+                break;
+        }
+        return true;
+    }
+
+    private void scrollDown(ScrollView s){
+        final ScrollView scrollView = s;
+        scrollView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.fullScroll(View.FOCUS_DOWN );
+            }
+        }, 1000);
+    }
+
+    private void drawActivity (){
+
+    }
+
+    private void drawBackground() {
+
+    }
+    private void drawTowers(){
+
     }
 }
