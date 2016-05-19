3bfa3d7436584e4b7e5b418360633bf45d8f5232
Jason Lie Yung Tjong <Jtlie@live.nl>
2016-05-19 14:36:00 +0200

    Unit test setup Preference  class

--- app/src/test/java/com/jenky/codebuddy/ExampleUnitTest.java
+++ app/src/test/java/com/jenky/codebuddy/ExampleUnitTest.java
@@ -1,15 +1,26 @@
-package com.example.codebuddy.codebuddy;
+package com.jenky.codebuddy;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import android.content.Context;
 
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
+
+    private static final String FAKE_STRING = "HELLO WORLD";
+
+    @Mock
+    Context mMockContext;
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void readStringFromContext_LocalizedString() {
+        // Given a mocked Context injected into the object under test...
+
     }
 }
