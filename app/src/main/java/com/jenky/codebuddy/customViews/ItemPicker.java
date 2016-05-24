30c2bf9b03c2cabd4180b7a0781e7a582853be53
Jtlie <jtlie@live.nl>
2016-05-24 22:51:30 +0200

    start equipment

---
+++ app/src/main/java/com/jenky/codebuddy/customViews/ItemPicker.java
@@ -0,0 +1,14 @@
+package com.jenky.codebuddy.customViews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.NumberPicker;
+
+/**
+ * Created by JTLie on 24-5-2016.
+ */
+public class ItemPicker extends NumberPicker {
+    public ItemPicker(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ app/src/main/java/com/jenky/codebuddy/ui/activities/EquipmentActivity.java
@@ -0,0 +1,18 @@
+package com.jenky.codebuddy.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jenky.codebuddy.R;
+import com.jenky.codebuddy.util.TestData;
+
+/**
+ * Created by JTLie on 24-5-2016.
+ */
+public class EquipmentActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equipment);
+    }
+}
